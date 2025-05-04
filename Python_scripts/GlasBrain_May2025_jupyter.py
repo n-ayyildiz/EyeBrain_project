236a7849ebcdfea7a06f788a29501de9c2cdd201
nayyildiz <nazifeayyildiz@gmail.com>
2025-05-04 20:27:43 +0200

    updated glass brain with Mango red overlay color bar to be consistent among imaged

--- Python_scripts/GlasBrain_May2025_jupyter.py
+++ Python_scripts/GlasBrain_May2025_jupyter.py
@@ -2,19 +2,36 @@
 # coding: utf-8
 
 # In[ ]:
-
-
 #import libraries
 from nilearn import plotting
 import nibabel as nib
 import matplotlib.pyplot as plt
-
+import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
 
 # In[6]:
+    # Mango red_overlay color range exported
+    # 'mango_red_overlay.lut' file
+    # Define the normalized positions and RGB values (converted to [0, 1])
+mango_lut_positions = [0.0, 0.5, 0.95, 1.0]
+mango_lut_colors = [
+    (191/255, 0/255, 0/255),      # Dark red
+    (255/255, 127/255, 0/255),    # Orange
+    (255/255, 255/255, 0/255),    # Yellow
+    (255/255, 255/255, 255/255)   # White (alpha = 0 originally)
+]
 
+# Create the custom colormap
+mango_red_overlay = LinearSegmentedColormap.from_list(
+    'mango_red_overlay',
+    list(zip(mango_lut_positions, mango_lut_colors)),
+    N=256
+)
+
+#%%
 
 #load nifti data
-data = nib.load('path/to/results_avG_wCVR.nii')
+data = nib.load('path/to/avg_TNFLT.nii')
 
 
 # In[7]:
@@ -24,14 +41,7 @@ data = nib.load('path/to/results_avG_wCVR.nii')
 data2 = nib.load('path/to/results_avG_woutCVR.nii')
 
 
-# In[ ]:
-
-
-
-
-
 # In[21]:
-
 
 # Visualize the image using a glass brain plot 
 plotting.plot_glass_brain(data,
@@ -39,7 +49,7 @@ plotting.plot_glass_brain(data,
                           #display_mode='ortho',  # You can try 'ortho', 'triplanar', etc.
                           colorbar=True,  # Show the colorbar
                           #plot_abs=False,  # If you want to display absolute values (usually useful for contrast)
-                          cmap='hot', #'YlOrRd_r',
+                          cmap=mango_red_overlay, #'YlOrRd_r', 'hot'
                           threshold=3.1,
                           #vmax=6.2,  # Set maximum value to 6.2 for the color scale
                           vmin=3.1,
@@ -47,18 +57,17 @@ plotting.plot_glass_brain(data,
 
 # Show the plot
 #plt.show()
-plt.savefig("avG_wCVR_glass_6apr2025.svg", format='svg')
+plt.savefig("avG_wCVR_glass_4May2025.svg", format='svg')
 
 
 # In[22]:
-
 
 # Visualize the image using a glass brain plot 
 plotting.plot_glass_brain(data2,
                           # title="Glass Brain Visualization",
                           #display_mode='ortho',  # You can try 'ortho', 'triplanar', etc.
                           colorbar=True,  # Show the colorbar
-                          cmap='hot', #'YlOrRd_r',
+                          cmap=mango_red_overlay, #'YlOrRd_r', 'hot'
                           #plot_abs=False,  # If you want to display absolute values (usually useful for contrast)
                           threshold=3.1,
                           vmin=3.1,
@@ -66,18 +75,15 @@ plotting.plot_glass_brain(data2,
 
 # Show the plot
 #plt.show()
-plt.savefig("avG_woutCVR_glass_6apr2025.svg", format='svg')
+plt.savefig("avG_woutCVR_glass_4May2025.svg", format='svg')
 
 
 # In[5]:
 
-
-import matplotlib.pyplot as plt
-print(plt.colormaps())
+# to see other color options defined in python
+# import matplotlib.pyplot as plt
+# print(plt.colormaps())
 
 
 # In[ ]:
-
-
-
 
